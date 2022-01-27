d40d12040835d8658429083b31434201524a84c6
josemodernmcguire <77705870+josemodernmcguire@users.noreply.github.com>
2022-01-27 07:58:03 -0800

    first commit

---
+++ defs.py
@@ -0,0 +1,7 @@
+API_KEY = "4dc112e924dc0fca747ebf70f783cb2f-c3eadf268c16bc76a2274cb4e55f2bde"
+ACCOUNT_ID = "101-001-21480363-001"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}'
+}
