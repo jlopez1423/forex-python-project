7630af1b3b727ad992409ef9fe9dcea18cd4076c
jlopez1423 <jalopez1423@gmail.com>
2022-04-03 17:19:17 -0700

    off shoot commit

--- instrument.py
+++ instrument.py
@@ -21,6 +21,20 @@ class Instrument():
         df = cls.get_instruments_df()
         return [Instrument(x) for x in df.to_dict(orient='records')]
 
+    @classmethod
+    def get_instruments_dict(cls):
+        i_list = cls.get_instruments_list()
+        i_keys = [x.name for x in i_list]
+
+        return { k:v for (k,v) in zip(i_keys, i_list) }
+
+    @classmethod
+    def get_instrument_by_name(cls, pairname):
+        d = cls.get_instruments_dict()
+        if pairname in d:
+            return d[pairname]
+        else:
+            return None
 
 if __name__ == "__main__":
-   print(Instrument.get_instruments_list())
+    print(Instrument.get_instrument_by_name('EUR_USD'))
